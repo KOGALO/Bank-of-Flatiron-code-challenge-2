32e1154effacfb392daf7bb6607aee0461357c2d
kepherogalo <kepher.ogalo@student.moringaschool.com>
2023-06-09 14:11:55 +0300

    Setting add and fetch Transactions

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,36 @@ import AddTransactionForm from "./AddTransactionForm";
 import AccountContainer from "./AccountContainer";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
+  const [transactions, setTransaction] = useState([]);
+
+  const [handleSearch] = (searchTerm) => {
+    setSearchTerm(searchTerm);
+  }; 
+
+  const fetchTransactions = () => {
+    fetch("http://localhost:8001/transactions")
+    .then((Response)=> Response.json())
+    .then ((data) =>setTransactions(data))
+    .catch((error) => console.error("Error fetching transactions:", error))      
+  };
+
+  const AddTransaction = (newTransaction) => {
+    fetch("http://localhost:8001/transactions"), {
+      method: "POST",
+      Headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  }
+
+
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
         <h2>The Royal Bank of Flatiron</h2>
+        <Search onSearch={handleSearch}/>
+        <AddTransactionForm onAddTransaction={onAddTransaction}/>
       </div>
       <AccountContainer />
     </div>
